ea454f4354e74f8d00d8c55d78923935ea5b95c8
amnhh <2570079254@qq.com>
2017-03-21 13:42:24 +0800

    add all of it

--- jsonp.js
+++ jsonp.js
@@ -17,6 +17,10 @@
     callbackFunc : null
   };
 
+  // 成功和失败的 state
+  var STATE_SUCCESS = 1;
+  var STATE_FAIL = -1;
+
   /**
    * 清除函数的命名空间
    * @param  {String} name 函数名
@@ -31,6 +35,16 @@
   }
 
   /**
+   * 清除插入到页面内的 script 标签
+   * @param  {String} id 用于选定 script 标签的 id
+   * @return {Undefined}    重视过程而非返回值
+   */
+  function clearScriptById (id) {
+    var _script = document.getElementById(id);
+    window.document.head.removeChild(_script);
+  }
+
+  /**
    * jsonPromise 主体函数
    * @param {String} url -> 请求地址
    * @param {Object} config -> 用于覆盖默认配置的配置信息
@@ -39,7 +53,59 @@
   function jsonPromise (url, config) {
     // 把两个揉在一起, 后覆盖前
     var _config = Object.assign(defaultConfig, config);
+    // 最大延时时间
+    var timeout = _config.timeout;
+    // jsonpcallback 的名字
+    var jsonpcallback = _config.jsonpcallback;
+    var timeoutTag;
 
+    return new Promise(function (resolve, reject) {
+      // 确定 callbackFunc 的名字
+      var callbackFunc = _config.callbackFunc
+        ? _config.callbackFunc
+        : `${String(Math.random())}${Date.now()}`.replace(/\d\.\d{6}/, 'jsonp_');
+      // 确定 script 标签的 id
+      var scriptId = `${_config.jsonpcallback}_${callbackFunc}`;
+
+      window[callbackFunc] = function (response) {
+        resolve({
+          state : STATE_SUCCESS,
+          data : response
+        });
+
+        // 成功调用的话, 就把延时队列里面那个 reject 清除掉
+        if (timeoutTag) {
+          clearTimeout(timeoutTag);
+        }
+
+        // 移除 script 标签
+        clearScriptById(scriptId);
+
+        // 移除全局方法
+        clearFuncNamespace(callbackFunc);
+      };
+
+      // 给 url 添加 jsonpcallback 参数
+      url += (url.indexOf('?') === -1) ? '?' : '&';
+
+      // 创建 script 标签
+      var jsonpScriptTag = document.createElement('script');
+      // 设置 src 属性, 并把 callbackFunc 和 jsonpcallback 拼上去
+      jsonpScriptTag.setAttribute('src', `${url}${jsonpcallback}=${callbackFunc}`);
+      // 设置 id
+      jsonpScriptTag.setAttribute('id', scriptId);
+      // 插入 dom
+      document.head.appendChild(jsonpScriptTag);
+
+      timeoutTag = setTimeout(function () {
+        reject(`JSONP request timeout`);
+        // 移除 script 标签
+        clearScriptById(scriptId);
+
+        // 移除全局方法
+        clearFuncNamespace(callbackFunc);
+      }, timeout);
+    });
   }
 
 
